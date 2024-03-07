c13bf5e19ae7838f8d4cc917aa37c23c64f92a54
Muhammed Hashim T <87515404+MuhammedHashimT@users.noreply.github.com>
2024-03-07 05:27:00 +0000

    calculator design and script added

---
+++ script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('answer');
+
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
